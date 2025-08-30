f43ccf850c3b9d046dbd25d41af40e08d4d4f491
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-08-30 18:34:52 -0300

    Create Problema de Hadamard b.c

---
+++ Problema de Hadamard b.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+  // os dois produtos
+  int U[101];
+  int V[101];
+
+  // o resultado
+  int res[101];
+
+  // ler o primeiro vetor
+  for (int i = 0; i < 101; i++) {
+    scanf("%d", &U[i]);
+    if (U[i] > 100) {
+      printf("INVALIDO\n");
+    }
+  }
+
+  // ler o segundo vetor 
+  for (int i = 0; i < 101; i++) {
+    scanf("%d", &V[i]);
+    if (V[i] > 100) {
+      printf("INVALIDO\n");
+    }
+  }
+
+  // calcular, armazenar e exibir o valor do produto
+  for (int i = 0; i < 101; i++) {
+    res[i] = U[i] * V[i];
+    printf("U[%d] = %d\n", i, res[i]);
+  }
+  return 0;
+}
