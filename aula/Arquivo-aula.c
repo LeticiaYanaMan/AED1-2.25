3df618e461d38e53d721264e6a12e739778020c4
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-09-16 14:36:09 -0300

    Update Arquivo-aula.c

--- aula/Arquivo-aula.c
+++ aula/Arquivo-aula.c
@@ -10,3 +10,17 @@ int main() {
 }
 
 // vai funcionar e abrir um file de texto chamado data.txt
+-----------------------------------------------------------------------------
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("data.txt", "w");
+    if (file == NULL) {
+        printf("ERRO\n");
+        return 1;
+    }
+    fprintf(file, "Nome: %s, Idade: %d\n", "Alice", 25);
+    return 0;
+}
+
+// vai funcionar e, no arquivo aberto, escrever exatamente "Nome: Alice, Idade: 25" e pula uma linha
