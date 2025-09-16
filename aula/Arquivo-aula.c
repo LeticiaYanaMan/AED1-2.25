d8cdd2b494500f852ea6a394264e2faed83713fd
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-09-16 14:41:08 -0300

    Update Arquivo-aula.c

--- aula/Arquivo-aula.c
+++ aula/Arquivo-aula.c
@@ -24,3 +24,28 @@ int main() {
 }
 
 // vai funcionar e, no arquivo aberto, escrever exatamente "Nome: Alice, Idade: 25" e pula uma linha
+
+-------------------------------------------------------------------------
+// EXERCICIO 1 - SLIDE 47 DA AULA 3-4
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("log.txt", "a");
+    if (file == NULL) {
+        printf("ERRO\n");
+        return 1;
+    }
+    fprintf(file, "Execução concluída com sucesso");
+    fclose(file);
+    return 0;
+}
+
+// EXERCICIO 2 - SLIDE 47 DA AULA 3-4
+
+
+
+
+
+
+
+
