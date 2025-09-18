84308a136c82d621a66ad8bb0451258dbd9ce2d8
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-09-18 12:43:25 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,21 +1,50 @@
-Exercício:
-Crie um programa em C que defina uma struct chamada Produto, contendo:
+// Exercício:
+// Crie um programa em C que defina uma struct chamada Produto, contendo:
 
-char nome[50]
-float preco
-int quantidade
+// char nome[50]
+// float preco
+// int quantidade
 
-O programa deve:
-1. Declarar um vetor de 5 produtos.
-2. Ler os dados de cada produto utilizando ponteiros para struct.
-3. Imprimir todos os produtos cadastrados.
-4. Mostrar o nome do produto com o maior valor total em estoque (preço × quantidade).
----------------------------------------------------------------------
+// O programa deve:
+// 1. Declarar um vetor de 5 produtos.
+// 2. Ler os dados de cada produto utilizando ponteiros para struct.
+// 3. Imprimir todos os produtos cadastrados.
+// 4. Mostrar o nome do produto com o maior valor total em estoque (preço x quantidade).
+///////////////////////////////////
 
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+typedef struct {
   char nome[50];
+  float preco;
+  int quantidade;
+} Produto;
+
+int main() {
+  Produto p1[6];
+  Produto *p; // ponteiro que aponta para struct
+
+
+  for (int i = 0; i < 6; i++) {
+    p = &p1[i]; // p aponta para o primeiro produto 
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
