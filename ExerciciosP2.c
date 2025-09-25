2a1c11a878071b8ccf7560fe4de1546a65d9e62a
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-09-25 13:54:46 -0300

    Update ExerciciosP2.c

--- ExerciciosP2.c
+++ ExerciciosP2.c
@@ -1,5 +1,14 @@
 // 23/09
 // 1. Peça ao usuário um vetor de n elementos inteiros. Depois, duplique o tamanho incluíndo os mesmos elementos com sinal negativo. Imprima o vetor ordenado.
+realloc(P, sizeof(int)* tam*2); //tam é tamanho
+for
+P(tam+i) = -P[i];
+
 
 // 2. Dado um arquivo de texto que contém uma sequência de números inteiros, com comprimento desconhecido, faça uma função que armazena esses numeros em um vetor cujo tamanho seja exatamente o da sequencia de numeros. 
 // IMPORTANTE: Leia o arquivo uma só vez!
+tam = 0;
+while (fscanf("%d", &x));
+  p = realloc(p, sizeof(int)*(++tam));
+  p[tam-1] = x;
+
