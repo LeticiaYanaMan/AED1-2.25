23edc7a50af6d8fd9f493bd06916ac1fdca5da8b
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-09-25 13:04:28 -0300

    Create ExerciciosP2.c

---
+++ ExerciciosP2.c
@@ -0,0 +1,5 @@
+// 23/09
+// 1. Peça ao usuário um vetor de n elementos inteiros. Depois, duplique o tamanho incluíndo os mesmos elementos com sinal negativo. Imprima o vetor ordenado.
+
+// 2. Dado um arquivo de texto que contém uma sequência de números inteiros, com comprimento desconhecido, faça uma função que armazena esses numeros em um vetor cujo tamanho seja exatamente o da sequencia de numeros. 
+// IMPORTANTE: Leia o arquivo uma só vez!
