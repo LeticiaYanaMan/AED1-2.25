981bd883760c7c99e17d3c80b79e2e222441e8e7
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-09-30 14:20:41 -0300

    Update 30 de setembro (extra).py

--- aula/30 de setembro (extra).py
+++ aula/30 de setembro (extra).py
@@ -1,6 +1,7 @@
 print("Hello World!") # equivale a desde o include <stdio.h> até o return 0 de um codigo em C que faz a mesma coisa --- não precisa declarar biblioteca
+#alem de ser mais facil de escrever, ocupa menos linhas 
 
------------------------------------------------
+##########################################
 
 def maixmo(a, b): #os blocos começam com : --- não precisa colocar chaves, nem noc meço e nem precisa indiciar fim --- a indentação é bem importante 
   if a > b:
@@ -8,7 +9,7 @@ def maixmo(a, b): #os blocos começam com : --- não precisa colocar chaves, nem
   else:
     return b # o proprio codigo reconhece se for int, char --- voce n precisa escrever como em C
 
----------------------------------------------
+##########################################
 
 def potencia(a, b):
   prod = 1
@@ -16,7 +17,7 @@ def potencia(a, b):
     prod = a * prod
   return prod
 
---------------------------------------------
+##########################################
 
 print("Entre com a e b")
 a = int(input()) #quando voce recebe uma varivael, precisa dizer qual tipo de dado voce quer que ele seja declarado
@@ -26,12 +27,18 @@ pot = potencia(a, b)
 print("Maior: ", maior)
 print("a ^b: ", pot)
 
---------------------------------------------
+##########################################
 
+print("Digite 10 números")
+lista = [] 
+for i in tange(10):
+  lista.append(int(input())) # vai adicionando ao vetor de acordo com a quanntidade colocada no range --- não precisa declarar o vetor já definidamente como 10
+print("Positivos")
+for x in lista:
+  if x > 0: # para ver se é maior que 0 
+    print(x)
 
-
-
-
+######################################
 
 
 
