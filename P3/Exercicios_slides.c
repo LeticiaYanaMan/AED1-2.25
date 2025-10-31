968964c5888f95596419cb4b4a4a438528143cf5
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-10-31 19:37:58 -0300

    Add function to remove last element from stack
    Implement function to remove the last element from a stack.

---
+++ P3/Exercicios_slides.c
@@ -0,0 +1,7 @@
+// P3 - 1
+
+// versão interativa do copiar lista
+// versão recursiva do inverter lista
+// versão interativa do concatenar lista
+
+// implementar a função de remover o ultimo elemento (pilha)
