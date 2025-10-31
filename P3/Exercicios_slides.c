cabe917cc868a2ebf7db7ffdc83842e34e875c2d
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-10-31 19:55:25 -0300

    Add comment for removing last element in lists
    Add comment for implementing removal function in doubly linked lists.

--- P3/Exercicios_slides.c
+++ P3/Exercicios_slides.c
@@ -5,3 +5,5 @@
 // versão interativa do concatenar lista
 
 // implementar a função de remover o ultimo elemento (pilha)
+
+// implementar a função de remoção do ultimo elemento (listas duplamente encadeadas)
