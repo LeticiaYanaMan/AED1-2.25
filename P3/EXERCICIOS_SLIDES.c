e3800299d91b7fc15cbf1fb103c786bdc06b965f
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-10-31 21:32:38 -0300

    Update and rename Exercicios_slides.c to EXERCICIOS_SLIDES.c

---
+++ P3/EXERCICIOS_SLIDES.c
@@ -0,0 +1,21 @@
+// P3 - 1
+
+// versão interativa do copiar lista
+// versão recursiva do inverter lista
+// versão interativa do concatenar lista
+
+// implementar a função de remover o ultimo elemento (pilha)
+
+// implementar a função de remoção do ultimo elemento (listas duplamente encadeadas)
+
+// implementar a função de inserção no inicio (lista duplamente encadeada circular)
+// implementar a função de remoção do ultimo elemento (lista duplamente encadeada circular)
+
+// EXERCICIOS PRATICOS
+  // implemente uma função para contar os elementos da lista;
+  // adicione uma função para inserir em uma posição específica;
+  // modifique a função de busca para retornar a posição do elemento;
+  // implemente uma função que faça concatenação de listas;
+  // implemente uma função para imprimir todos os elementos da lista;
+
+// P3 - 2
--- P3/Exercicios_slides.c
+++
@@ -1,9 +0,0 @@
-// P3 - 1
-
-// versão interativa do copiar lista
-// versão recursiva do inverter lista
-// versão interativa do concatenar lista
-
-// implementar a função de remover o ultimo elemento (pilha)
-
-// implementar a função de remoção do ultimo elemento (listas duplamente encadeadas)
