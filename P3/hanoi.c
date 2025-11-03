408323b58a738e3f9d3175ae8b462cbd363c2023
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-11-03 17:18:30 -0300

    Update hanoi.c

--- P3/hanoi.c
+++ P3/hanoi.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void hanoi(int d, char og, char meio, char fim){
+void hanoi(int d, char og, char fim, char meio){
   if(d==1){
     printf("Move de %d para %d", og, fim);
     return;
   }
-  else {
     hanoi(d-1, og, meio, fim); //para mover da torre A para B, tendo C como intermediario
     printf("Move o disco %d de %c para %c", d, og, fim);
-    hanoi(d-1, meio, og, fim); //move de B para C, agora A esta no meio
-  }
+    hanoi(d-1, meio, fim, og); //move de B para C, agora A esta no meio
 }
 
 int main(){
   int n;
+  printf("Numero de discos: ");
   scanf("%d", &n);
   hanoi(n, 'A', 'B', 'C');
-  printf("/n");
+  printf("\n");
+  return 0;
 }  
