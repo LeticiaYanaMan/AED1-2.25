6bb9b7599c703e4201576e4a04076d189c0f4d52
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-11-03 17:12:09 -0300

    Update hanoi.c

--- P3/hanoi.c
+++ P3/hanoi.c
@@ -8,7 +8,7 @@ void hanoi(int d, char og, char meio, char fim){
   }
   else {
     hanoi(d-1, og, meio, fim); //para mover da torre A para B, tendo C como intermediario
-    printf("Move o disco %d de %c para %c", n, og, fim);
+    printf("Move o disco %d de %c para %c", d, og, fim);
     hanoi(d-1, meio, og, fim); //move de B para C, agora A esta no meio
   }
 }
