c7cdbcc954d063abc206182306ec138b4654813c
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-11-26 15:59:10 -0300

    Create Menor.c

---
+++ P4/Menor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct No {
+  int chave;
+  struct No* esq;
+  struct No* dir;
+} No;
+
+int main() {
+  No* raiz == NULL;
+  char c[50];
+int valor;
+  while (scanf("%d", c) != EOF) {
+    if (strcmp(c, "ADD")==0)  {
+      scanf("%d", &valor);
+      raiz = INSERT(raiz, valor);
+    }
+    if (strcmp(c, "PRINT")==0) {
+      PRINT(raiz);
